81722adba81b5ef3943d6b10ab512f8f4031b82c
Your Name <caihebatista@gmail.com>
2023-10-21 14:06:37 -0300

    admin adap

--- __tests__/api/board.spec.ts
+++ __tests__/api/board.spec.ts
@@ -1,19 +1,19 @@
 import request from 'supertest'
 import { app } from '../../src/app'
 import { describe, expect, test } from '@jest/globals';
-import { MOCKUP } from '../../src/utils/mockup';
+import { avisos } from '../../src/utils/mockup';
 
 describe('As rotas de aviso devem funcionar corretamente', () => {
     test('Deve criar avisos com sucesso 1', async () => {
-        const response = await request(app).post('/aviso/').send(MOCKUP[0])
+        const response = await request(app).post('/aviso/').send(avisos[0])
         expect(response.statusCode).toBe(200)
     })
     test('Deve criar avisos com sucesso 2', async () => {
-        const response = await request(app).post('/aviso/').send(MOCKUP[1])
+        const response = await request(app).post('/aviso/').send(avisos[1])
         expect(response.statusCode).toBe(200)
     })
     test('Deve criar avisos com sucesso 3', async () => {
-        const response = await request(app).post('/aviso/').send(MOCKUP[2])
+        const response = await request(app).post('/aviso/').send(avisos[2])
         expect(response.statusCode).toBe(200)
     })
     test('Deve receber os avisos com sucesso', async () => {
--- src/controllers/createAdmin/createAdminController.ts
+++ src/controllers/createAdmin/createAdminController.ts
@@ -4,9 +4,9 @@ import { AppError } from "../../config/error";
 
 export default class CreateAdminController {
     async handle(req: Request, res: Response) {
-        const { name, password } = req.body
+        const { name, password, can_use_zap } = req.body
         const usecase = new CreateAdminUseCase()
-        const response = await usecase.execute({ name,password })
+        const response = await usecase.execute({ name,password, can_use_zap })
         if (!response) throw new AppError('Algo deu errado na criação de administrador.')
         res.status(201).json({ message: 'Usuário Criado!' })
     }
